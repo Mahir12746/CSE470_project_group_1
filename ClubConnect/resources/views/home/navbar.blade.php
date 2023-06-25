780b8dffce3d7044bbf05f8de65e4934b429aee9
Tahsin Ashrafee <tahsinashrafee99@gmail.com>
2023-06-26 05:24:02 +0600

    Fixed Logout

--- ClubConnect/resources/views/home/navbar.blade.php
+++ ClubConnect/resources/views/home/navbar.blade.php
@@ -1,63 +1,63 @@
 <nav class="navbar navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand" href="index.html">
-                    Club Connect
-                </a>
+    <div class="container">
+        <a class="navbar-brand" href="index.html">
+            Club Connect
+        </a>
 
-                <a href="ticket.html" class="btn custom-btn d-lg-none ms-auto me-4">Buy Ticket</a>
+        <a href="ticket.html" class="btn custom-btn d-lg-none ms-auto me-4">Buy Ticket</a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_1">Home</a>
-                        </li>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_1">Home</a>
+                </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_2">Clubs</a>
-                        </li>
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_2">Clubs</a>
+                </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_3">Players</a>
-                        </li>
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_3">Players</a>
+                </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_4">Matches</a>
-                        </li>
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_4">Matches</a>
+                </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_5">Search</a>
-                        </li>
-                        @if (Route::has('login'))
+                <li class="nav-item">
+                    <a class="nav-link click-scroll" href="#section_5">Search</a>
+                </li>
 
-                        @auth
-                        <li class="nav-item">
-                           <x-app-layout>
-  
-                           </x-app-layout>
-                        </li>
+                @if (Route::has('login'))
+                    @auth
+                    <!-- Display user-related options -->
+                    <li class="nav-item">
+                        <a class="btn custom-btn d-lg-block d-none" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                    @else
+                    <!-- Display login and registration options -->
+                    <li class="nav-item">
+                        <a class="btn custom-btn d-lg-block d-none" style="margin-right: 10px"
+                            href="{{ route('login') }}">Login</a>
+                    </li>
 
-                        @else
-
-                        <li class="nav-item">
-                           <a class="btn custom-btn d-lg-block d-none" style="margin-right: 10px" href="{{ route('login') }}">Login</a>
-                        </li>
-
-                        <li class="nav-item">
-                           <a class="btn custom-btn d-lg-block d-none" href="{{ route('register') }}">Register</a>
-                        </li>
-
-                        @endauth
-
-                        @endif
-
-                    </ul>
-                    <!-- <a href="ticket.html" class="btn custom-btn d-lg-block d-none">Buy Ticket</a> -->
-                    
-                </div>
-            </div>
-        </nav>
+                    <li class="nav-item">
+                        <a class="btn custom-btn d-lg-block d-none" href="{{ route('register') }}">Register</a>
+                    </li>
+                    @endauth
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
